8d2538298cdf4474d97b0fece21f95f20a42fd1d
nerjib <kabirnajib0@gmail.com>
2022-01-14 23:15:53 +0100

    files isert

--- src/controllers/files.js
+++ src/controllers/files.js
@@ -22,6 +22,50 @@ router.get('/', async (req, res) => {
     }
   });  
   
+  router.post('/', upload.single('file'),  async(req, res) => {
+    const uploader = async (path) => await cloudinary.uploads(path,'zazzaufiles', req.body.filename+'_'+(new Date()).getTime());
+
+    if (req.method === 'POST') {
+        const urls = []
+        const file = req.file.path;
+    //    for (const file of files) {
+       //   const { path } = file;
+          const newPath = await uploader(file)
+          urls.push(newPath.url)
+         // fs.unlinkSync(path)
+      //  }
+    
+   // cloudinary.uploader.upload(req.file.path, async (result)=> {
+    
+    const createUser = `INSERT INTO
+      files(filename,folder,category,date,uploadedby,fileurl)
+      VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`;
+    console.log(req.body)
+    const values = [
+    req.body.filename,
+    req.body.folder,
+    req.body.category,
+    moment(new Date()),
+    req.body.uploadedby,
+    urls[0]
+      ];
+    try {
+    const { rows } = await db.query(createUser, values);
+    // console.log(rows);
+    return res.status(201).send(rows);
+    } catch (error) {
+    return res.status(400).send(error);
+    }
+  
+  //  },{ resource_type: "auto", public_id: `ridafycovers/${req.body.title}` })
+
+} else {
+    res.status(405).json({
+      err: `${req.method} method not allowed`
+    })
+  }
+
+  });
 
 
 
