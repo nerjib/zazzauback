0a8e591d06e9bf9f45846b44fe612ab6acffa9c1
nerjib <kabirnajib0@gmail.com>
2022-01-18 09:53:39 +0100

    insert guarantor

--- src/controllers/customer.js
+++ src/controllers/customer.js
@@ -38,13 +38,37 @@ router.get('/', async (req, res) => {
     
    // cloudinary.uploader.upload(req.file.path, async (result)=> {
     
-    const createUser = `INSERT customer
-        (name)
-      VALUES ($1) RETURNING *`;
+    const createUser = `INSERT INTO customer
+        (name,lastname,othername,dob,state,lga,pow,gradelevel,ministry,psn,department,bank,actno,branch,phone,modeofpayment,
+            caddress, phaddress, proposedlayout, plotno, plotsize, date,formid,imgurl)
+      VALUES ($1, $2, $3, $4, $5, $6, $7,$8, $9, $10,$11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24) RETURNING *`;
     console.log(req.body)
     const values = [
-    req.body.name
-         ];
+    req.body.name,
+    req.body.lastname,
+    req.body.othername,
+    req.body.dob,
+    req.body.state,
+    req.body.lga,
+    req.body.pow,
+    req.body.gradelevel,
+    req.body.ministry,
+    req.body.psn,
+    req.body.department,
+    req.body.bank,
+    req.body.actno,
+    req.body.branch,
+    req.body.phone,
+    req.body.modeofpayment,
+    req.body.caddress,
+    req.body.phaddress,
+    req.body.proposedlayout,
+    req.body.plotno,
+    req.body.plotsize,
+    moment(new Date()),
+    req.body.formid,
+    urls[0] 
+      ];
     try {
     const { rows } = await db.query(createUser, values);
     // console.log(rows);
